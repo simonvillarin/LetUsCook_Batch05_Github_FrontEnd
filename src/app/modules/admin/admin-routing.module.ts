b699319c123ae59de0d7ae80770027520d66451b
simonvillarin <simon.villarin@gmail.com>
2023-06-25 15:08:19 +0800

    admin routing

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CourseComponent } from './pages/course/course.component';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { ProgramComponent } from './pages/program/program.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/home',
+    component: HomeComponent,
+  },
+  {
+    path: 'admin/course',
+    component: CourseComponent,
+  },
+  {
+    path: 'admin/curriculum',
+    component: CurriculumComponent,
+  },
+  {
+    path: 'admin/program',
+    component: ProgramComponent,
+  },
+  {
+    path: 'admin/schedule',
+    component: ScheduleComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class AdminRoutingModule {}
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgramComponent } from './pages/program/program.component';
 import { CourseComponent } from './pages/course/course.component';
-import { CurriculumComponent } from './curriculum/curriculum.component';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
 import { HomeComponent } from './pages/home/home.component';
-
-
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [
-    DashboardComponent,
     ProgramComponent,
     CourseComponent,
-    CurriculumComponent,
     ScheduleComponent,
-    HomeComponent
+    HomeComponent,
+    CurriculumComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
