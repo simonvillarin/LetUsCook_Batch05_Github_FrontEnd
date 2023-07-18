7857d523b7f121751adcb2e4aceecbe030eb7c65
Embhurrrr <andreiembersanmiguel@gmail.com>
2023-07-18 13:35:39 +0800

    email input page and create new password page

--- src/app/modules/forgot-password/forgot-password.module.ts
+++ src/app/modules/forgot-password/forgot-password.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { EmailInputComponent } from './pages/email-input/email-input.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 @NgModule({
-  declarations: [ForgotPasswordComponent],
+  declarations: [ForgotPasswordComponent, EmailInputComponent, ResetPasswordComponent],
   imports: [CommonModule, ForgotPasswordRoutingModule, SharedModule],
 })
 export class ForgotPasswordModule {}
